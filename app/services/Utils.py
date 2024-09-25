201449dd9940703a3dea265ffdd858923c6da21d
Rafael Oliveira <contact@rafaeloliveiradesign.com>
2024-09-25 10:49:02 -0300

    Refactor slugify method in Utils.py

--- app/services/Utils.py
+++ app/services/Utils.py
@@ -2,27 +2,44 @@ import re
 
 
 class Utils:
-    def __init__(self):
-        pass
+    @staticmethod
+    def slugify(string: str) -> str:
+        """
+        Convert a given string to a URL-friendly 'slug'.
 
-    def slugify(self, string: str) -> str:
+        Steps:
+        - Convert to lowercase and replace spaces with hyphens.
+        - Replace accented characters with ASCII equivalents.
+        - Remove non-alphanumeric characters, keeping only letters, numbers, and hyphens.
+        - Remove leading/trailing hyphens and reduce multiple hyphens to one.
+
+        Args:
+            string (str): Input string to be slugified.
+
+        Returns:
+            str: Slugified version of the input string.
+        """
         # Convert to lowercase and replace spaces with hyphens
         slug = string.lower().replace(" ", "-")
 
-        # Replace special characters with their closest ASCII equivalent
-        slug = re.sub(r"[àáâãäå]", "a", slug)
-        slug = re.sub(r"[èéêë]", "e", slug)
-        slug = re.sub(r"[ìíîï]", "i", slug)
-        slug = re.sub(r"[òóôõö]", "o", slug)
-        slug = re.sub(r"[ùúûü]", "u", slug)
-        slug = re.sub(r"[ñ]", "n", slug)
-        slug = re.sub(r"[ç]", "c", slug)
+        # Replace accented characters with their ASCII equivalents
+        accents_mapping = {
+            r"[àáâãäå]": "a",
+            r"[èéêë]": "e",
+            r"[ìíîï]": "i",
+            r"[òóôõö]": "o",
+            r"[ùúûü]": "u",
+            r"[ñ]": "n",
+            r"[ç]": "c",
+        }
+        for pattern, replacement in accents_mapping.items():
+            slug = re.sub(pattern, replacement, slug)
 
         # Remove any characters that are not alphanumeric or hyphens
         slug = re.sub(r"[^a-z0-9-]", "", slug)
 
-        # Ensure that consecutive hyphens are reduced to a single hyphen
+        # Reduce consecutive hyphens to a single hyphen
         slug = re.sub(r"-+", "-", slug)
 
-        # Strip hyphens from the beginning and end
+        # Strip hyphens from the beginning and end of the string
         return slug.strip("-")
